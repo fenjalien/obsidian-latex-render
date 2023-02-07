963e7b9c295c8b88ab4929b1cf859de8c503bb5c
Jack <34489450+fenjalien@users.noreply.github.com>
2023-02-07 02:25:39 +0000

    working system

--- main.ts
+++ main.ts
@@ -1,85 +1,42 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Md5 } from 'ts-md5';
+import * as fs from 'fs';
+import * as temp from 'temp';
+import * as path from 'path';
+import { exec } from 'child_process';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	pdflatexCommand: fs.PathLike,
+	pdf2svgCommand: fs.PathLike,
+	timeout: number,
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	pdflatexCommand: "pdflatex",
+	pdf2svgCommand: "pdf2svg2",
+	timeout: 10000,
+
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	svgFolder = this.app.vault.configDir + "\\latex-svg-renders";
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		if (!(await this.app.vault.adapter.exists(this.svgFolder)).valueOf()) {
+			this.app.vault.createFolder(this.svgFolder);
+		}
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerMarkdownCodeBlockProcessor("latex", (s, c) => this.renderLatexToContainer(s, c));
 	}
 
 	onunload() {
-
+		if (this.app.vault.adapter.exists(this.svgFolder).valueOf()) {
+			this.app.vault.adapter.rmdir(this.svgFolder, true);
+		}
 	}
 
 	async loadSettings() {
@@ -89,21 +46,50 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+
+	renderLatexToContainer(source: string, container: HTMLElement) {
+		return new Promise<void>((resolve, reject) => {
+			this.renderLatexToSVG(source).then((v: string) => {
+				container.innerHTML = v;
+				resolve();
+			}
+			).catch(err => { container.innerHTML = err; reject(err); });
+		});
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	renderLatexToSVG(source: string) {
+		return new Promise(async (resolve, reject) => {
+			source = "\\documentclass{standalone}\n" + source;
+			let md5Hash = Md5.hashStr(source);
+			let basePath = (this.app.vault.adapter as FileSystemAdapter).getBasePath();
+			let localCwd = path.join(this.svgFolder, md5Hash);
+			let cwd = path.join(basePath, localCwd);
+			let svgFile = md5Hash + ".svg";
+			let svgPath = path.join(this.svgFolder, svgFile);
+			let texFile = md5Hash + ".tex";
+			let pdfFile = md5Hash + ".pdf";
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+			if (await this.app.vault.adapter.exists(svgPath)) {
+				resolve(svgPath)
+			} else {
+				await this.app.vault.adapter.mkdir(localCwd);
+				await this.app.vault.create(path.join(localCwd, texFile), source);
+				exec(
+					`"${this.settings.pdflatexCommand}" -interaction=nonstopmode -halt-on-error -shell-escape "${texFile}" && "${this.settings.pdf2svgCommand}" "${path.join(cwd, pdfFile)}" "${cwd}"`,
+					{ timeout: this.settings.timeout, cwd: cwd },
+					async (err, stdout, stderr) => {
+						if (err) reject([err, stdout, stderr]);
+						else {
+							// await this.app.vault.adapter.rename(path.join(localCwd, "content1.svg"), svgPath);
+							let svgData = await this.app.vault.adapter.read(path.join(localCwd, "content1.svg"));
+							await this.app.vault.adapter.rmdir(localCwd, true);
+							resolve(svgData);
+						};
+					},
+				);
+			}
+		});
 	}
 }
 
@@ -116,21 +102,26 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('pdflatex Command')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.pdflatexCommand.toString())
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.pdflatexCommand = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('pdf2svg Command')
+			.addText(text => text
+				.setValue(this.plugin.settings.pdf2svgCommand.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.pdf2svgCommand = value;
 					await this.plugin.saveSettings();
 				}));
 	}
